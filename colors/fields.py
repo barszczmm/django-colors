82fe4cb864c59509761d7159e837b5205c1ac41d
Jaap Roes <jaap@eight.nl>
2011-09-23 09:47:07 +0200

    South can now introspect the field, http://code.google.com/p/django-colors/issues/detail?id=2

--- colors/fields.py
+++ colors/fields.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.db import models
 from django import forms
 from django.utils.text import capfirst
 
 
 from colors.widgets import ColorPickerWidget
+
+
+if 'south' in settings.INSTALLED_APPS:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules((), ["^colors\.fields\.ColorField"])
+
 
 class ColorField(models.CharField):
     def __init__(self, *args, **kwargs):
